473e3aadf623dc6cdd62ab4a682ae4560994808d
DRSenabled <139079132+DRSenabled@users.noreply.github.com>
2023-08-20 12:00:52 -0400

    Update CIS261 Course Project Phase 2.py

--- CIS261 Course Project Phase 2.py
+++ CIS261 Course Project Phase 2.py
@@ -1,20 +1,13 @@
 import datetime
 
 def calculate_tax_and_netpay(total_hours, hourly_rate, tax_rate):
-    tax = total_hours * hourly_rate * (tax_rate / 100)
-    net_pay = total_hours * hourly_rate - tax
+    tax = float(total_hours) * float(hourly_rate) * (float(tax_rate) / 100)
+    net_pay = float(total_hours) * float(hourly_rate) - tax
     return tax, net_pay
 
 def get_name():
     name = input("Enter employee name: ")
     return name
-
-def get_from_and_to_date():
-    from_date = input("Enter from date in mm/dd/yyyy format: ")
-    to_date = input("Enter to date in mm/dd/yyyy format: ")
-    from_date = datetime.datetime.strptime(from_date, "%m/%d/%Y").date()
-    to_date = datetime.datetime.strptime(to_date, "%m/%d/%Y").date()
-    return from_date, to_date
 
 def get_total_hours():
     total_hours = float(input("Enter total hours: "))
@@ -29,7 +22,7 @@ def get_tax_rate():
     return tax_rate
 
 def get_gross_pay(total_hours, hourly_rate):
-    gross_pay = total_hours * hourly_rate
+    gross_pay = float(total_hours) * float(hourly_rate)
     return gross_pay
 
 def display_employee_info(from_date, to_date, name, total_hours, hourly_rate, tax_rate, tax, gross_pay, net_pay):
@@ -54,55 +47,51 @@ def display_total_info(total_dict):
     print("Total net pay:", total_dict['total_net_pay'])
     print("----------------------------------------------------")
 
-def main():
-    employee_list = []
-    total_dict = {
-        "total_employees": 0,
-        "total_hours": 0,
-        "total_tax": 0,
-        "total_gross_pay": 0,
-        "total_net_pay": 0
-    }
+def get_from_and_to_date():
+    from_date = input("Enter from date in mm/dd/yyyy format: ")
+    from_date = datetime.datetime.strptime(from_date, "%m/%d/%Y").date()
+    to_date = input("Enter to date in mm/dd/yyyy format: ")
+    to_date = datetime.datetime.strptime(to_date, "%m/%d/%Y").date()
+    return from_date, to_date
 
-    while True:
-        from_date, to_date = get_from_and_to_date()
-        name = get_name()
-        if name.lower() == "end":
-            break
+def calculate_employees_taxes(employee_list, total_dict):
+    for employee in employee_list:
+        from_date, to_date, name, hours, hourly_rate, tax_rate = employee
+        gross_pay = get_gross_pay(hours, hourly_rate)
+        tax, net_pay = calculate_tax_and_netpay(hours, hourly_rate, tax_rate)
+        display_employee_info(from_date, to_date, name, hours, hourly_rate, tax_rate, tax, gross_pay, net_pay)
 
-        total_hours = get_total_hours()
-        hourly_rate = get_hourly_rate()
-        tax_rate = get_tax_rate()
-        
-        gross_pay = get_gross_pay(total_hours, hourly_rate)
-        tax, net_pay = calculate_tax_and_netpay(total_hours, hourly_rate, tax_rate)
-
-        employee_info = {
-            "from_date": from_date,
-            "to_date": to_date,
-            "name": name,
-            "total_hours": total_hours,
-            "hourly_rate": hourly_rate,
-            "tax_rate": tax_rate,
-            "tax": tax,
-            "gross_pay": gross_pay,
-            "net_pay": net_pay
-        }
-
-        employee_list.append(employee_info)
-
+        # update total
         total_dict['total_employees'] += 1
-        total_dict['total_hours'] += total_hours
+        total_dict['total_hours'] += hours
         total_dict['total_tax'] += tax
         total_dict['total_gross_pay'] += gross_pay
         total_dict['total_net_pay'] += net_pay
 
-    for employee_info in employee_list:
-        display_employee_info(employee_info["from_date"], employee_info["to_date"], employee_info["name"],
-                              employee_info["total_hours"], employee_info["hourly_rate"], employee_info["tax_rate"],
-                              employee_info["tax"], employee_info["gross_pay"], employee_info["net_pay"])
+def main():
+    employee_list = []
+    total_dict = {"total_employees": 0, "total_hours": 0, "total_tax": 0, "total_gross_pay": 0, "total_net_pay": 0}
 
+    while True:
+        name = get_name()
+
+        if name == "End":
+            break
+
+        # Placing date inputs before checking for "End" to ensure consistency
+        from_date, to_date = get_from_and_to_date()
+
+        hours = get_total_hours()
+        hourly_rate = get_hourly_rate()
+        tax_rate = get_tax_rate()
+
+        print()  # Just to start printing in a new line once one user data insertion is completed
+        employee_list.append([from_date, to_date, name, hours, hourly_rate, tax_rate])
+
+    calculate_employees_taxes(employee_list, total_dict)
+    # Display total counts
     display_total_info(total_dict)
 
+# Import guard
 if __name__ == "__main__":
     main()
