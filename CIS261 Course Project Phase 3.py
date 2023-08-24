50789c53654b7f43ee554b9b1bfe584c7380d4ac
DRSenabled <139079132+DRSenabled@users.noreply.github.com>
2023-08-24 15:30:25 -0400

    Update CIS261 Course Project Phase 3.py

--- CIS261 Course Project Phase 3.py
+++ CIS261 Course Project Phase 3.py
@@ -47,27 +47,73 @@ def display_total_info(total_dict):
     print("Total net pay:", total_dict['total_net_pay'])
     print("----------------------------------------------------")
 
-def get_from_and_to_date():
-    from_date = input("Enter from date in mm/dd/yyyy format: ")
-    from_date = datetime.datetime.strptime(from_date, "%m/%d/%Y").date()
-    to_date = input("Enter to date in mm/dd/yyyy format: ")
-    to_date = datetime.datetime.strptime(to_date, "%m/%d/%Y").date()
-    return from_date, to_date
+def get_from_date():
+    while True:
+        from_date = input("Enter from date in mm/dd/yyyy format (or 'All' for all records): ")
+        if from_date.lower() == "all" or is_valid_date(from_date):
+            return from_date
+        else:
+            print("Invalid date format. Please use mm/dd/yyyy or 'All'.")
 
-def write_employee_info_to_file(employee_info, file_name):
-    with open(file_name, "a") as file:
-        file.write(f"{employee_info[0]}|{employee_info[1]}|{employee_info[2]}|{employee_info[3]}|{employee_info[4]}|{employee_info[5]}\n")
+def is_valid_date(date_str):
+    try:
+        datetime.datetime.strptime(date_str, "%m/%d/%Y")
+        return True
+    except ValueError:
+        return False
 
-def calculate_employees_taxes(employee_list, total_dict):
-    for employee in employee_list:
-        from_date, to_date, name, hours, hourly_rate, tax_rate = employee
-        gross_pay = get_gross_pay(hours, hourly_rate)
-        tax, net_pay = calculate_tax_and_netpay(hours, hourly_rate, tax_rate)
-        display_employee_info(from_date, to_date, name, hours, hourly_rate, tax_rate, tax, gross_pay, net_pay)
+def process_records(file_path, from_date):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
 
-        # update total
-        total_dict['total_employees'] += 1
-        total_dict['total_hours'] += hours
-        total_dict['total_tax'] += tax
-        total_dict['total_gross_pay'] += gross_pay
-        total_dict['total_net_pay'] +=
+    total_dict = {"total_employees": 0, "total_hours": 0, "total_tax": 0, "total_gross_pay": 0, "total_net_pay": 0}
+
+    for line in lines:
+        record = line.strip().split("|")
+        record_from_date = datetime.datetime.strptime(record[0], "%m/%d/%Y").date()
+
+        if from_date.lower() == "all" or from_date == record_from_date:
+            from_date = record_from_date
+            to_date = datetime.datetime.strptime(record[1], "%m/%d/%Y").date()
+            name = record[2]
+            hours = float(record[3])
+            hourly_rate = float(record[4])
+            tax_rate = float(record[5])
+
+            gross_pay = get_gross_pay(hours, hourly_rate)
+            tax, net_pay = calculate_tax_and_netpay(hours, hourly_rate, tax_rate)
+            display_employee_info(from_date, to_date, name, hours, hourly_rate, tax_rate, tax, gross_pay, net_pay)
+
+            total_dict['total_employees'] += 1
+            total_dict['total_hours'] += hours
+            total_dict['total_tax'] += tax
+            total_dict['total_gross_pay'] += gross_pay
+            total_dict['total_net_pay'] += net_pay
+
+    display_total_info(total_dict)
+
+def main():
+    file_path = "employee_records.txt"
+
+    while True:
+        name = get_name()
+
+        if name == "End":
+            break
+
+        from_date, to_date = get_from_and_to_date()
+
+        hours = get_total_hours()
+        hourly_rate = get_hourly_rate()
+        tax_rate = get_tax_rate()
+
+        with open(file_path, "a") as file:
+            file.write(f"{from_date}|{to_date}|{name}|{hours}|{hourly_rate}|{tax_rate}\n")
+
+        print()  # To start printing in a new line 
+
+    from_date_to_report = get_from_date()
+    process_records(file_path, from_date_to_report)
+
+if __name__ == "__main__":
+    main()
