efee50d24faf4b366208bac7142fe051d3d54725
DRSenabled <139079132+DRSenabled@users.noreply.github.com>
2023-08-24 15:54:47 -0400

    Update CIS261 Course Project Phase 3.py

--- CIS261 Course Project Phase 3.py
+++ CIS261 Course Project Phase 3.py
@@ -1,47 +1,39 @@
 import datetime
 
-class EmployeeRecord:
-    def __init__(self, from_date, to_date, name, hours, hourly_rate, tax_rate):
-        self.from_date = from_date
-        self.to_date = to_date
-        self.name = name
-        self.hours = hours
-        self.hourly_rate = hourly_rate
-        self.tax_rate = tax_rate
-
-def write_record_to_file(file_path, record):
-    with open(file_path, "a") as file:
-        file.write(f"{record.from_date}|{record.to_date}|{record.name}|{record.hours}|{record.hourly_rate}|{record.tax_rate}\n")
-
-def read_records_from_file(file_path):
-    records = []
-    with open(file_path, "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            record_data = line.strip().split("|")
-            from_date = datetime.datetime.strptime(record_data[0], "%m/%d/%Y").date()
-            to_date = datetime.datetime.strptime(record_data[1], "%m/%d/%Y").date()
-            name = record_data[2]
-            hours = float(record_data[3])
-            hourly_rate = float(record_data[4])
-            tax_rate = float(record_data[5])
-            records.append(EmployeeRecord(from_date, to_date, name, hours, hourly_rate, tax_rate))
-    return records
-
 def calculate_tax_and_netpay(total_hours, hourly_rate, tax_rate):
-    tax = total_hours * hourly_rate * (tax_rate / 100)
-    net_pay = total_hours * hourly_rate - tax
+    tax = float(total_hours) * float(hourly_rate) * (float(tax_rate) / 100)
+    net_pay = float(total_hours) * float(hourly_rate) - tax
     return tax, net_pay
 
-def display_employee_info(record, tax, net_pay):
+def get_name():
+    name = input("Enter employee name: ")
+    return name
+
+def get_total_hours():
+    total_hours = float(input("Enter total hours: "))
+    return total_hours
+
+def get_hourly_rate():
+    hourly_rate = float(input("Enter hourly rate: "))
+    return hourly_rate
+
+def get_tax_rate():
+    tax_rate = float(input("Enter tax rate (in %): "))
+    return tax_rate
+
+def get_gross_pay(total_hours, hourly_rate):
+    gross_pay = float(total_hours) * float(hourly_rate)
+    return gross_pay
+
+def display_employee_info(from_date, to_date, name, total_hours, hourly_rate, tax_rate, tax, gross_pay, net_pay):
     print("----------------------------------------------------")
-    print("From date:", record.from_date.strftime('%m/%d/%Y'))
-    print("To date:", record.to_date.strftime('%m/%d/%Y'))
-    print("Employee name:", record.name)
-    print("Total hours:", record.hours)
-    print("Hourly rate:", record.hourly_rate)
-    print("Gross pay:", record.hours * record.hourly_rate)
-    print("Tax rate:", record.tax_rate)
+    print("From date:", from_date.strftime('%m/%d/%Y'))
+    print("To date:", to_date.strftime('%m/%d/%Y'))
+    print("Employee name:", name)
+    print("Total hours:", total_hours)
+    print("Hourly rate:", hourly_rate)
+    print("Gross pay:", gross_pay)
+    print("Tax rate:", tax_rate)
     print("Income tax:", tax)
     print("Net pay:", net_pay)
     print("----------------------------------------------------")
@@ -70,45 +62,58 @@ def is_valid_date(date_str):
     except ValueError:
         return False
 
-def process_records(records):
+def process_records(file_path, from_date):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+
     total_dict = {"total_employees": 0, "total_hours": 0, "total_tax": 0, "total_gross_pay": 0, "total_net_pay": 0}
-    
-    from_date_to_report = get_from_date()
-    
-    for record in records:
-        if from_date_to_report.lower() == "all" or from_date_to_report == record.from_date:
-            tax, net_pay = calculate_tax_and_netpay(record.hours, record.hourly_rate, record.tax_rate)
-            display_employee_info(record, tax, net_pay)
-            
+
+    for line in lines:
+        record = line.strip().split("|")
+        record_from_date = datetime.datetime.strptime(record[0], "%m/%d/%Y").date()
+
+        if from_date.lower() == "all" or from_date == record_from_date:
+            from_date = record_from_date
+            to_date = datetime.datetime.strptime(record[1], "%m/%d/%Y").date()
+            name = record[2]
+            hours = float(record[3])
+            hourly_rate = float(record[4])
+            tax_rate = float(record[5])
+
+            gross_pay = get_gross_pay(hours, hourly_rate)
+            tax, net_pay = calculate_tax_and_netpay(hours, hourly_rate, tax_rate)
+            display_employee_info(from_date, to_date, name, hours, hourly_rate, tax_rate, tax, gross_pay, net_pay)
+
             total_dict['total_employees'] += 1
-            total_dict['total_hours'] += record.hours
+            total_dict['total_hours'] += hours
             total_dict['total_tax'] += tax
-            total_dict['total_gross_pay'] += record.hours * record.hourly_rate
+            total_dict['total_gross_pay'] += gross_pay
             total_dict['total_net_pay'] += net_pay
-            
+
     display_total_info(total_dict)
 
 def main():
     file_path = "employee_records.txt"
-    records = read_records_from_file(file_path)
 
     while True:
-        name = input("Enter employee name: ")
+        name = get_name()
 
         if name == "End":
             break
 
-        from_date = input("Enter from date in mm/dd/yyyy format: ")
-        to_date = input("Enter to date in mm/dd/yyyy format: ")
-        hours = float(input("Enter total hours: "))
-        hourly_rate = float(input("Enter hourly rate: "))
-        tax_rate = float(input("Enter tax rate (in %): "))
+        from_date, to_date = get_from_and_to_date()
 
-        record = EmployeeRecord(from_date, to_date, name, hours, hourly_rate, tax_rate)
-        write_record_to_file(file_path, record)
-        print()
+        hours = get_total_hours()
+        hourly_rate = get_hourly_rate()
+        tax_rate = get_tax_rate()
 
-    process_records(records)
+        with open(file_path, "a") as file:
+            file.write(f"{from_date}|{to_date}|{name}|{hours}|{hourly_rate}|{tax_rate}\n")
+
+        print()  # Just to start printing in a new line once one user data insertion is completed
+
+    from_date_to_report = get_from_date()
+    process_records(file_path, from_date_to_report)
 
 if __name__ == "__main__":
     main()
