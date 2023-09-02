fdc8c0c92d73d77f6f012258519b87e819178460
DRSenabled <139079132+DRSenabled@users.noreply.github.com>
2023-09-02 13:47:10 -0400

    Update CIS261 Course Project Phase 3.py

--- CIS261 Course Project Phase 3.py
+++ CIS261 Course Project Phase 3.py
@@ -101,35 +101,45 @@ def ReadEmployeeInformation(fromdate):
     for employee in data:
         
         employee = [x.strip() for x in employee.strip().split("|")]
+        
         if not condition:
             EmpDetailList.append(
                 [employee[0], employee[1], employee[2],
 float(employee[3]), float(employee[4]), float(employee[5])])
         else:
             if fromdate == employee[0]:
-            EmpDetailList.append(
-[employee[0], employee[1], employee[2],
+                EmpDetailList.append(
+                    [employee[0], employee[1], employee[2],
 float(employee[3]), float(employee[4]), float(employee[5])])
-return EmpDetailList
+
+    return EmpDetailList
+    
 if __name__ == "__main__":
-EmpDetailList = []
-EmpTotals = {}
-while True:
-empname = GetEmpName()
-if (empname.upper() == "END"):
-break
-fromdate, todate = GetDatesWorked()
-hours = GetHoursWorked()
-hourlyrate = GetHourlyRate()
-taxrate = GetTaxRate()
-print()
-EmpDetail = [fromdate, todate, empname, hours, hourlyrate,
+
+    EmpDetailList = []
+    EmpTotals = {}
+
+    while True:
+        empname = GetEmpName()
+        if (empname.upper() == "END"):
+            break
+            
+        fromdate, todate = GetDatesWorked()
+        hours = GetHoursWorked()
+        hourlyrate = GetHourlyRate()
+        taxrate = GetTaxRate()
+
+        print()
+
+        EmpDetail = [fromdate, todate, empname, hours, hourlyrate,
 taxrate]
-WriteEmployeeInformation(EmpDetail)
+        WriteEmployeeInformation(EmpDetail)
 print()
 print()
 fromdate = GetFromDate()
+
 EmpDetailList = ReadEmployeeInformation(fromdate)
+
 print()
 printinfo(EmpDetailList)
 print()
